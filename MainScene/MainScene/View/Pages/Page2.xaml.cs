a4a19449c2ee85099bc98fd2ed098c5b7b360709
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2020-10-14 14:46:08 +0900

    [ADD] Assets

--- MainScene/MainScene/View/Pages/Page2.xaml.cs
+++ MainScene/MainScene/View/Pages/Page2.xaml.cs
@@ -3,6 +3,8 @@ using System;
 using KFC_Project.Model;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 
 namespace MainScene.View.Pages
 {
@@ -13,19 +15,22 @@ namespace MainScene.View.Pages
     {
         private List<Product> foodProduct = new List<Product>()
         {
-            new Product(){ Category = CategoryEnum.Bugger, name = "치킨버거", Image = @"/Assets/기네스머쉬룸와퍼.png" },
+            new Product(){ Category = CategoryEnum.Bugger, name = "치킨버거", Image = @"/Assets/ch.png" },
+            new Product(){ Category = CategoryEnum.Bugger, name = "치킨버거", Image = @"/Assets/ch.png" },
+            new Product(){ Category = CategoryEnum.Drink, name = "콜라", Image = @"/Assets/cola.png" },
+            new Product(){ Category = CategoryEnum.Drink, name = "사이다", Image = @"/Assets/cyida.png" },
+            new Product(){ Category = CategoryEnum.Side, name = "닭껍질 튀김", Image = @"/Assets/crustch.png" },
+            new Product(){ Category = CategoryEnum.Side, name = "치킨 텐더", Image = @"/Assets/tender.png" },
         };
         public Page2()
         {
             InitializeComponent();
+            lbMenus.ItemsSource = foodProduct.Where(x => x.Category == CategoryEnum.Bugger).ToList();
         }
 
  
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
 
-        }
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
@@ -34,6 +39,21 @@ namespace MainScene.View.Pages
            new Uri("View/Pages/Page3.xaml", UriKind.Relative)
 
            );
+        }
+
+        private void lbCategory_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {   
+           ListBoxItem lbi = ((sender as ListBox).SelectedItem as ListBoxItem);
+           if(lbi.Content.ToString() == "버거")
+            {
+              lbMenus.ItemsSource = foodProduct.Where(x => x.Category == CategoryEnum.Bugger).ToList();
+            }else if(lbi.Content.ToString() == "음료")
+            {
+                lbMenus.ItemsSource = foodProduct.Where(x => x.Category == CategoryEnum.Drink).ToList();
+            }else if(lbi.Content.ToString() == "사이드 메뉴")
+            {
+                lbMenus.ItemsSource = foodProduct.Where(x => x.Category == CategoryEnum.Side).ToList();
+            }
         }
     }
 }
